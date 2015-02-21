6f906260ac670759d2dc360abe3352256c466bc0
William Yang <williamyang93@gmail.com>
2015-02-20 18:48:48 -0800

    Trying to commit to master

--- GUI/UAV-Forge-New/debug/moc_missionrecap.cpp
+++ GUI/UAV-Forge-New/debug/moc_missionrecap.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'missionrecap.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.0)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'missionrecap.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.4.0. It"
+#error "This file was generated using the moc from 5.3.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -29,14 +29,13 @@ struct qt_meta_stringdata_MissionRecap_t {
     )
 static const qt_meta_stringdata_MissionRecap_t qt_meta_stringdata_MissionRecap = {
     {
-QT_MOC_LITERAL(0, 0, 12), // "MissionRecap"
-QT_MOC_LITERAL(1, 13, 20), // "replayMissionClicked"
-QT_MOC_LITERAL(2, 34, 0), // ""
-QT_MOC_LITERAL(3, 35, 21), // "on_backButton_clicked"
-QT_MOC_LITERAL(4, 57, 21), // "on_playButton_clicked"
-QT_MOC_LITERAL(5, 79, 21), // "on_stopButton_clicked"
-QT_MOC_LITERAL(6, 101, 23) // "on_stopButton_2_clicked"
-
+QT_MOC_LITERAL(0, 0, 12),
+QT_MOC_LITERAL(1, 13, 20),
+QT_MOC_LITERAL(2, 34, 0),
+QT_MOC_LITERAL(3, 35, 21),
+QT_MOC_LITERAL(4, 57, 21),
+QT_MOC_LITERAL(5, 79, 21),
+QT_MOC_LITERAL(6, 101, 23)
     },
     "MissionRecap\0replayMissionClicked\0\0"
     "on_backButton_clicked\0on_playButton_clicked\0"
@@ -83,7 +82,7 @@ void MissionRecap::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
         case 1: _t->on_backButton_clicked(); break;
         case 2: _t->on_playButton_clicked(); break;
         case 3: _t->on_stopButton_clicked(); break;
-        case 4: _t->on_stopButton_2_clicked(); break;
+        //case 4: _t->on_stopButton_2_clicked(); break;
         default: ;
         }
     }
@@ -92,7 +91,7 @@ void MissionRecap::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
 
 const QMetaObject MissionRecap::staticMetaObject = {
     { &QWidget::staticMetaObject, qt_meta_stringdata_MissionRecap.data,
-      qt_meta_data_MissionRecap,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+      qt_meta_data_MissionRecap,  qt_static_metacall, 0, 0}
 };
 
 
@@ -103,7 +102,7 @@ const QMetaObject *MissionRecap::metaObject() const
 
 void *MissionRecap::qt_metacast(const char *_clname)
 {
-    if (!_clname) return Q_NULLPTR;
+    if (!_clname) return 0;
     if (!strcmp(_clname, qt_meta_stringdata_MissionRecap.stringdata))
         return static_cast<void*>(const_cast< MissionRecap*>(this));
     return QWidget::qt_metacast(_clname);
