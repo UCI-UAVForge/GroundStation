278b57179fda8326badb5b28e089195f6139ca7c
Kevin Phan <kphan4@uci.edu>
2015-02-21 15:21:20 -0800

    Implemented rapidjson

--- Network/jsonmsg.cpp
+++ Network/jsonmsg.cpp
@@ -1,16 +1,24 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include "rapidjson/prettywriter.h"
+using namespace rapidjson;
+
+
 
 struct Msg{
-	std::string userName;
-	std::string msg;
+	std::string  userName;
+	std::string  msg;
 };
 
 class jsonObj{
 public:
 	jsonObj();
-	void addVar(std::string vName, std::string val);
+	void addVar(struct Msg);
 	std::string extractString();
 private:
 	std::vector<std::string> varName;
@@ -20,9 +28,11 @@ private:
 jsonObj::jsonObj(){
 }
 
-void jsonObj::addVar(std::string vName, std::string val){
-	varName.push_back(vName);
-	value.push_back(val);
+void jsonObj::addVar(struct Msg command){
+//	varName.push_back("userName");
+	varName.push_back(command.userName);
+//	value.push_back("value");
+	value.push_back(command.msg);
 }
 
 std::string jsonObj::extractString(){
@@ -30,8 +40,10 @@ std::string jsonObj::extractString(){
 	int varSize = varName.size();
 	jsonMsg.append("{");
 	for (int i = 0; i < varSize; i++){
+		jsonMsg.append("\"");
 		jsonMsg.append(varName[i]);
-		jsonMsg.append(":\"");
+		jsonMsg.append("\"");
+		jsonMsg.append(" : \"");
 		jsonMsg.append(value[i]);
 		jsonMsg.append("\",");
 	}
@@ -51,13 +63,22 @@ int main()
 	std::cout << "Message contains: " << std::endl <<
 		my_message.userName << std::endl << my_message.msg << std::endl << std::endl;
 
-	jo.addVar("userName", my_message.userName);
-	jo.addVar("msg", my_message.msg);
+	jo.addVar(my_message);
 
-	std::string jsonStr = jo.extractString();
+	const char* jsonStr = jo.extractString().data();
 
 	std::cout << "Json string: " << std::endl << jsonStr << std::endl;
+	rapidjson::Document document;
+	document.Parse(jsonStr);
 
+	rapidjson::StringBuffer buffer;
+    rapidjson::PrettyWriter<StringBuffer> writer(buffer); //Pretty Optional
+	document.Accept(writer);;
+	std::cout << "rapidjson version:" << std::endl;
+	std::cout << buffer.GetString() << std::endl;
+	std::cout << document["Steve"].GetString() << std::endl;
 
 	return 0;
 }
+
+
