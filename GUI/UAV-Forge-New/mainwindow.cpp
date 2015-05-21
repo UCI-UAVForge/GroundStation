9cdef8f55c5f4fd3114bf3c67ddfd051757c210b
Efrain Coyt <ecoyt@uci.edu>
2015-05-20 20:06:44 -0700

    A lot of buttons changes

--- GUI/UAV-Forge-New/mainwindow.cpp
+++ GUI/UAV-Forge-New/mainwindow.cpp
@@ -18,6 +18,25 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     //ui->pushButton_8->setIcon(QIcon(":/res/longButtons/Mission_Execution_Button_Type_2.png"));
     //ui->pushButton_8->setIconSize(QSize(181,101));
+    QPixmap pixmap(":/res/longButtons/Mission_Execution_Button_Type_2.png");
+    QIcon ButtonIcon(pixmap);
+    ui->pushButton_8->setIcon(ButtonIcon);
+    ui->pushButton_8->setIconSize(pixmap.rect().size()/13);//QSize(ui->pushButton_8->width()+100,ui->pushButton_8->height()+200));
+
+    QPixmap pixmap2(":/res/longButtons/Mission_Planning_Button_Type_2.png");
+    QIcon ButtonIcon2(pixmap2);
+    ui->pushButton_9->setIcon(ButtonIcon2);
+    ui->pushButton_9->setIconSize(pixmap2.rect().size()/13);
+
+    QPixmap pixmap3(":/res/longButtons/Mission_Recap_Button_Type_2.png");
+    QIcon ButtonIcon3(pixmap3);
+    ui->pushButton_7->setIcon(ButtonIcon3);
+    ui->pushButton_7->setIconSize(pixmap3.rect().size()/13);
+
+    QPixmap pixmap4(":/res/longButtons/Tutorial_Button_Type_2.png");
+    QIcon ButtonIcon4(pixmap4);
+    ui->pushButton_6->setIcon(ButtonIcon4);
+    ui->pushButton_6->setIconSize(pixmap4.rect().size()/13);
 }
 
 MainWindow::~MainWindow()
