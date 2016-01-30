4c30a10b8254efe223a51dd4ab18e63a990acb4d
Danny Ortega <dannyortega13@gmail.com>
2016-01-29 17:47:28 -0800

    Added print functions for the 4 types of packets

--- Simulation/QTUDPprototypes/SimulDraft2/SimulDraft2UAV/uav.h
+++ Simulation/QTUDPprototypes/SimulDraft2/SimulDraft2UAV/uav.h
@@ -6,6 +6,7 @@
 #include <QLabel>
 #include <QDialog>
 #include <QLineEdit>
+#include <QTextStream>
 
 #include "packet.h"
 #include "ackpacket.h"
@@ -19,6 +20,17 @@ class UAV : public QObject
 public:
     const static int length = 1000;
     UAV(QWidget *parent = 0);
+
+    /* Created Functions to print out information about packets
+     * parameters(Packet type packet)
+     * return void
+     * created by: Daniel Ortega
+     *
+     **/
+    void print_ack_packet(const Protocol::AckPacket& packet);
+    void print_action_packet(const Protocol::ActionPacket& packet);
+    void print_info_packet(const Protocol::InfoPacket& packet);
+    void print_telemtry_packet(const Protocol::TelemtryPacket& packet);
 
 private slots:
     void processPendingDatagrams();
