532a4be75f0f2e77ebe8c0405efa5ff80ac82f4b
UAVForge <uavforge@uci.edu>
2018-02-01 15:20:09 -0800

    revert main.cpp to 3c992d577efcf75c33e2ac44a670ca51b6e90067

--- GS/main.cpp
+++ GS/main.cpp
@@ -6,48 +6,27 @@
 
 #include "plan_mission.hpp"
 #include "point.hpp"
-#include <fenv.h>
-#include <rrt/BiRRT.hpp>
-inline int feenableexcept(unsigned int excepts)
-{
-    static fenv_t fenv;
-    unsigned int new_excepts = excepts & FE_ALL_EXCEPT;
-    // previous masks
-    unsigned int old_excepts;
-
-    if (fegetenv(&fenv)) {
-        return -1;
-    }
-    old_excepts = fenv.__control & FE_ALL_EXCEPT;
-
-    // unmask
-    fenv.__control &= ~new_excepts;
-    fenv.__mxcsr   &= ~(new_excepts << 7);
-
-    return fesetenv(&fenv) ? -1 : old_excepts;
-}
-
 int main(int argc, char *argv[]) {
-    feenableexcept(FE_INVALID);
-//    static const int splash_width = 600;
-//    static const int splash_height = 217;
-//    static const QString filename(":/res/images/UAV Forge Logo 2015.svg");
 
-//    QApplication a(argc, argv);
-//    //Construct and load the SVG image
-//    QSvgRenderer svg(filename);
-//    //Calculate the scaled size within a bounded size
-//    QSize SplashSize = svg.viewBox().size();
-//    SplashSize.scale(splash_width, splash_height, Qt::KeepAspectRatio);
-//    //Construct the pixmap to contain the SVG image
-//    QPixmap pixmap(SplashSize);
-//    pixmap.fill(Qt::white);
-//    QPainter painter(&pixmap);
-//    //Render the SVG to the pixmap
-//    svg.render(&painter, pixmap.rect());
-//    //Construct and show the splash screen
-//    QSplashScreen splash(pixmap);
-//    // splash.show();
+    static const int splash_width = 600;
+    static const int splash_height = 217;
+    static const QString filename(":/res/images/UAV Forge Logo 2015.svg");
+
+    QApplication a(argc, argv);
+    //Construct and load the SVG image
+    QSvgRenderer svg(filename);
+    //Calculate the scaled size within a bounded size
+    QSize SplashSize = svg.viewBox().size();
+    SplashSize.scale(splash_width, splash_height, Qt::KeepAspectRatio);
+    //Construct the pixmap to contain the SVG image
+    QPixmap pixmap(SplashSize);
+    pixmap.fill(Qt::white);
+    QPainter painter(&pixmap);
+    //Render the SVG to the pixmap
+    svg.render(&painter, pixmap.rect());
+    //Construct and show the splash screen
+    QSplashScreen splash(pixmap);
+    // splash.show();
     PlanMission pm;
     pm.add_goal_point(Point::fromGeodetic(38.14254, -76.43409, 0));
     pm.add_goal_point(Point::fromGeodetic(38.14468, -76.42799, 0));
@@ -84,12 +63,12 @@ int main(int argc, char *argv[]) {
     pm.set_obstacles(QJsonDocument::fromJson(sb.toUtf8()));
     qDebug() << pm.get_path(Point::fromGeodetic(38.14468, -76.42799, 0));
 //    MainMDIDisplay x;
-//    MainDockWindow dockWindow;
-//    //x.showNormal();
-//    dockWindow.showNormal();
-//    //Hide the splash screen.
-//    splash.finish(&dockWindow);
+    MainDockWindow dockWindow;
+    //x.showNormal();
+    dockWindow.showNormal();
+    //Hide the splash screen.
+    splash.finish(&dockWindow);
 
-//    return a.exec();
+    return a.exec();
 }
 
